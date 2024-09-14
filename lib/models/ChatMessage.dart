bc049ae3c9047d45c2ee07f7e82ebc6a4f5195ad
Vipul-sh01 <vipulsharma6021@gmail.com>
2024-09-15 00:27:29 +0530

    add chat futures

---
+++ lib/models/ChatMessage.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatMessage {
+  final String content;
+  final String sender;
+  final DateTime timestamp;
+
+  ChatMessage({
+    required this.content,
+    required this.sender,
+    required this.timestamp,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'content': content,
+      'sender': sender,
+      'timestamp': Timestamp.fromDate(timestamp),
+    };
+  }
+
+  factory ChatMessage.fromMap(Map<String, dynamic> map) {
+    return ChatMessage(
+      content: map['content'] ?? '',
+      sender: map['sender'] ?? '',
+      timestamp: (map['timestamp'] as Timestamp).toDate(),
+    );
+  }
+}
+// TODO Implement this library.
